d9228e1b2447fc02f38b2e005f2ec7fd8fcc07c3
Nilan <nilan.marktanner@student.kit.edu>
2016-05-04 11:05:30 +0200

    Basic working todo app.

---
+++ js/actions/index.js
@@ -0,0 +1,31 @@
+let nextTodoId = 0
+export const addTodo = (text) => {
+  return {
+    type: 'ADD_TODO',
+    id: nextTodoId++,
+    text
+  }
+}
+
+export const setVisibilityFilter = (filter) => {
+  return {
+    type: 'SET_VISIBILITY_FILTER',
+    filter
+  }
+}
+
+export const toggleTodo = (id) => {
+  return {
+    type: 'TOGGLE_TODO',
+    id
+  }
+}
+
+
+// Added
+export const deleteTodo = (id) => {
+  return {
+    type: 'DELETE_TODO',
+    id
+  }
+}
---
+++ js/app.js
@@ -0,0 +1,16 @@
+import 'babel-polyfill'
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import TodoApp from './components/TodoApp'
+
+let store = createStore(todoApp)
+
+render(
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>,
+  document.getElementById('root')
+)
