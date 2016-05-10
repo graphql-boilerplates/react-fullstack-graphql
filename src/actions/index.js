316b7690dfac7815f4bd2e50b1763223db201dc5
Nilan <nilan.marktanner@student.kit.edu>
2016-05-10 12:04:10 +0200

    Started using thunk middleware.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,33 @@
+import Model from '../models/model'
+
+const model = new Model()
+
 let nextTodoId = 0
-export const addTodo = (text) => {
+
+export const receiveTodos = (todos) => {
+  console.log('receive todo')
   return {
-    type: 'ADD_TODO',
-    id: nextTodoId++,
-    text,
+    type: 'RECEIVE_TODOS',
+    todos,
+  }
+}
+
+
+export const addTodo = (text) => {
+  console.log('add todo')
+  return function (dispatch) {
+    // First dispatch: the app state is updated to inform
+    // that the API call is starting.
+
+    // dispatch(requestPosts(subreddit))
+
+    // The function called by the thunk middleware can return a value,
+    // that is passed on as the return value of the dispatch method.
+
+    // In this case, we return a promise to wait for.
+    // This is not required by thunk middleware, but it is convenient for us.
+
+    return model.addTodo({text, complete: false}, (todos) => dispatch(receiveTodos(todos)))
   }
 }
 
