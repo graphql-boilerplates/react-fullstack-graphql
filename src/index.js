0ea09d21c7db77dba38ebd92d3837b0a7f0e1e40
Nikolas <nikolas.burk@gmail.com>
2017-04-16 13:51:28 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,8 +8,9 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 
 import './style.css'
 
-const networkInterface =
-  createNetworkInterface('https://api.graph.cool/simple/v1/__PROJECT_ID__')
+// Paste your endpoint for the Simple API here.
+// Info: https://github.com/graphcool-examples/react-apollo-todo-example#2-create-graphql-api-with-graphcool
+const networkInterface = createNetworkInterface({ uri: '__SIMPLE_API_ENDPOINT__' });
 
 // The x-graphcool-source header is to let the server know that the example app has started.
 // (Not necessary for normal projects)
