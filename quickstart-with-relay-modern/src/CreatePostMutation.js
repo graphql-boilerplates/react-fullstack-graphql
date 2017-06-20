d46f411dbe042e3b47a0a6692c564a76f97321e5
Nikolas <nikolas.burk@gmail.com>
2017-06-20 15:47:17 +0200

    Update CreatePostMutation.js

--- quickstart-with-relay-modern/src/CreatePostMutation.js
+++ quickstart-with-relay-modern/src/CreatePostMutation.js
@@ -34,6 +34,33 @@ export default function CreatePostMutation(description, imageUrl, callback) {
         callback()
       },
       onError: err => console.error(err),
+      optimisticUpdater: (proxyStore) => {
+        // 1 - create the `newPost` as a mock that can be added to the store
+        const id = 'client:newPost:' + tempID++
+        const newPost = proxyStore.create(id, 'Post')
+        newPost.setValue(id, 'id')
+        newPost.setValue(description, 'description')
+        newPost.setValue(imageUrl, 'imageUrl')
+
+        // 2 - add `newPost` to the store
+        const viewerProxy = proxyStore.get(viewerId)
+        const connection = ConnectionHandler.getConnection(viewerProxy, 'ListPage_allPosts')
+        if (connection) {
+          ConnectionHandler.insertEdgeAfter(connection, newPost)
+        }
+      },
+      updater: (proxyStore) => {
+        // 1 - retrieve the `newPost` from the server response
+        const createPostField = proxyStore.getRootField('createPost')
+        const newPost = createPostField.getLinkedRecord('post')
+
+        // 2 - add `newPost` to the store
+        const viewerProxy = proxyStore.get(viewerId)
+        const connection = ConnectionHandler.getConnection(viewerProxy, 'ListPage_allPosts')
+        if (connection) {
+          ConnectionHandler.insertEdgeAfter(connection, newPost)
+        }
+      },
     },
   )
 }
