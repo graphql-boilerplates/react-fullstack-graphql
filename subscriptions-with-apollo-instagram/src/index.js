9afdd0f4226e4b9bfa09220640da1184a3dbbf81
Nilan Marktanner <nilan.marktanner@gmail.com>
2017-07-07 21:03:47 +0200

    test 0.8.0 alpha

--- subscriptions-with-apollo-instagram/src/index.js
+++ subscriptions-with-apollo-instagram/src/index.js
@@ -9,11 +9,11 @@ import 'tachyons'
 import './index.css'
 
 // __SUBSCRIPTIONS_API_ENDPOINT__ looks similar to: `wss://subscriptions.graph.cool/v1/<PROJECT_ID>`
-const wsClient = new SubscriptionClient('__SUBSCRIPTIONS_API_ENDPOINT_')
+const wsClient = new SubscriptionClient('wss://subscriptions.graph.cool/v1/cizr2bzgvbp4d0127v4nst0ac')
 
 // __SIMPLE_API_ENDPOINT__ looks similar to: `https://api.graph.cool/simple/v1/<PROJECT_ID>`
 const networkInterface = createNetworkInterface({
-  uri: '__SIMPLE_API_ENDPOINT__',
+  uri: 'https://api.graph.cool/simple/v1/cizr2bzgvbp4d0127v4nst0ac',
 })
 
 const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
