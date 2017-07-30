352073eb99b20a3b676f352d87064c5e98fcdb90
Latik <latkovskyi@gmail.com>
2017-07-30 13:07:05 +0300

    add controls for navigation

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,15 +3,48 @@ import Post from './Post'
 import { createFragmentContainer, graphql } from 'react-relay'
 
 class PostList extends React.Component {
+  _loadAll = () => {
+    // here pagination action
+  }
+  _previousPage = () => {
+    // here pagination action
+  }
+
+  _nextPage = () => {
+    // here pagination action
+  }
+
   render() {
     const { viewer } = this.props
     return (
-      <div className="w-100 flex flex-column items-center">
-        {viewer.allPosts.edges.map(({ node }) => (
-          <div className="w-60" key={node.id}>
-            <Post post={node} />
-          </div>
-        ))}
+      <div>
+        <div className="w-100 flex flex-row justify-between">
+          <span
+            className="bg-white w-25 pa4 ttu dim black no-underline"
+            onClick={() => this._loadAll()}
+          >
+            Load All
+          </span>
+          <span
+            className="bg-white w-25 pa4 ttu dim black no-underline"
+            onClick={() => this._previousPage()}
+          >
+            Previous
+          </span>
+          <span
+            className="bg-white w-25 pa4 ttu dim black no-underline"
+            onClick={() => this._nextPage()}
+          >
+            Next
+          </span>
+        </div>
+        <div className="w-100 flex flex-column items-center">
+          {viewer.allPosts.edges.map(({ node }) => (
+            <div className="w-60" key={node.id}>
+              <Post post={node} />
+            </div>
+          ))}
+        </div>
       </div>
     )
   }
