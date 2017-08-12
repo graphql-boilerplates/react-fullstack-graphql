ba8969e875317bf813e2f2b20e300bde25a0a635
Jamie Barton <hi_jamie@me.com>
2017-08-12 13:36:56 +0100

    Updated to use ENV secrets + endpoint URI

--- subscriptions-with-apollo-instagram/src/index.js
+++ subscriptions-with-apollo-instagram/src/index.js
@@ -9,14 +9,14 @@ import 'tachyons'
 import './index.css'
 
 // __SUBSCRIPTIONS_API_ENDPOINT__ looks similar to: `wss://subscriptions.graph.cool/v1/<PROJECT_ID>`
-const wsClient = new SubscriptionClient('__SUBSCRIPTIONS_API_ENDPOINT_', {
+const wsClient = new SubscriptionClient(process.env.REACT_APP_GRAPHQL_SUBSCRIPTIONS_URI, {
   reconnect: true,
   timeout: 20000
 })
 
 // __SIMPLE_API_ENDPOINT__ looks similar to: `https://api.graph.cool/simple/v1/<PROJECT_ID>`
 const networkInterface = createNetworkInterface({
-  uri: '__SIMPLE_API_ENDPOINT__',
+  uri: process.env.REACT_APP_GRAPHQL_URI
 })
 
 const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
