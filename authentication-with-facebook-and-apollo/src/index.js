4cca346a27d9a421db0ab97952d0151035b59924
Martin Adams <heymartinadams@users.noreply.github.com>
2017-11-08 11:33:43 -0800

    Updated to new apollo-client 2.0

--- authentication-with-facebook-and-apollo/src/index.js
+++ authentication-with-facebook-and-apollo/src/index.js
@@ -4,27 +4,45 @@ import App from './components/App'
 import CreatePost from './components/CreatePost'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import 'tachyons'
-import { ApolloProvider, createNetworkInterface } from 'react-apollo'
+import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
+import { ApolloLink } from 'apollo-link'
+import { split } from 'apollo-link'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 
-
-const networkInterface = createNetworkInterface({ uri: '__SIMPLE_API_ENDPOINT__' })
-
-networkInterface.use([{
-  applyMiddleware (req, next) {
-    if (!req.options.headers) {
-      req.options.headers = {}
-    }
-
-    // get the authentication token from local storage if it exists
-    if (localStorage.getItem('graphcoolToken')) {
-      req.options.headers.authorization = `Bearer ${localStorage.getItem('graphcoolToken')}`
-    }
-    next()
-  },
-}])
-
-const client = new ApolloClient({ networkInterface })
+const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/__SIMPLE_API_ENDPOINT__' })
+const wsLink = new WebSocketLink({
+	uri: 'wss://subscriptions.graph.cool/v1/__SIMPLE_API_ENDPOINT__',
+	options: {
+		reconnect: true
+	}
+})
+const link = split(
+	({ query }) => {
+		const { kind, operation } = getMainDefinition(query)
+		return kind === 'OperationDefinition' && operation === 'subscription'
+	},
+	wsLink,
+	httpLink,
+)
+const middlewareLink = new ApolloLink((operation, forward) => {
+	const token = window.localStorage.getItem('AffiliateGenius:login')
+	const authorizationHeader = token ? `Bearer ${token}` : null
+	operation.setContext({
+		headers: {
+			authorization: authorizationHeader
+		}
+	})
+	return forward(operation)
+})
+const httpLinkWithAuthToken = middlewareLink.concat(link)
+const client = new ApolloClient({
+	link: httpLinkWithAuthToken,
+	cache: new InMemoryCache().restore(window.__APOLLO_STATE__),
+})
 
 ReactDOM.render((
     <ApolloProvider client={client}>
