c8e2d3c5b8a43fdeaa3b93a10a83fd2e59b7c925
Martin Adams <heymartinadams@users.noreply.github.com>
2017-11-09 15:03:18 -0800

    Removed websocket subscription link

--- authentication-with-facebook-and-apollo/src/index.js
+++ authentication-with-facebook-and-apollo/src/index.js
@@ -14,20 +14,6 @@ import { WebSocketLink } from 'apollo-link-ws'
 import { getMainDefinition } from 'apollo-utilities'
 
 const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/__SIMPLE_API_ENDPOINT__' })
-const wsLink = new WebSocketLink({
-  uri: 'wss://subscriptions.graph.cool/v1/__SIMPLE_API_ENDPOINT__',
-  options: {
-    reconnect: true
-  }
-})
-const link = split(
-  ({ query }) => {
-    const { kind, operation } = getMainDefinition(query)
-    return kind === 'OperationDefinition' && operation === 'subscription'
-  },
-  wsLink,
-  httpLink,
-)
 const middlewareLink = new ApolloLink((operation, forward) => {
   const token = window.localStorage.getItem('graphcoolToken')
   const authorizationHeader = token ? `Bearer ${token}` : null
@@ -38,7 +24,7 @@ const middlewareLink = new ApolloLink((operation, forward) => {
   })
   return forward(operation)
 })
-const httpLinkWithAuthToken = middlewareLink.concat(link)
+const httpLinkWithAuthToken = middlewareLink.concat(httpLink)
 const client = new ApolloClient({
   link: httpLinkWithAuthToken,
   cache: new InMemoryCache().restore(window.__APOLLO_STATE__),
