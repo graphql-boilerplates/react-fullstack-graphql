cb79c08191db55fd54ff4833898d21651890b787
Nikolas Burk <nikolas.burk@gmail.com>
2017-12-19 20:12:45 +0100

    renamed db schema

--- apollo-client/basic/install.js
+++ apollo-client/basic/install.js
@@ -1,10 +1,20 @@
 const fs = require('fs')
+const path = require('path')
+const commandExists = require('command-exists')
 
-module.exports = ({ project }) => {
+module.exports = async ({ project, projectPath }) => {
   const templateName = 'graphql-boilerplate'
   replaceInFile('server/src/index.js', templateName, project)
   replaceInFile('server/package.json', templateName, project)
   replaceInFile('server/graphcool.yml', templateName, project)
+
+  process.chdir(path.join(projectPath, 'server'))
+  if (commandExists.sync('yarn')) {
+    await shell('yarn install')
+  } else {
+    await shell('npm install')
+  }
+  process.chdir(projectPath)
 
   console.log(`\
 Next steps:
@@ -13,9 +23,8 @@ Next steps:
   2. Deploy database service: \`graphcool deploy\`
   3. Start local server: \`yarn start\` (you can now open a Playground at http://localhost:4000)
   4. Change directory: \`cd ..\`
-  5. Install dependencies: \`yarn install\` 
-  6. Start React app: \`yarn start\`
-  7. Open browser: http://localhost:3000
+  5. Start React app: \`yarn start\`
+  6. Open browser: http://localhost:3000
   `)
 }
 
@@ -26,4 +35,18 @@ function replaceInFile(filePath, searchValue, replaceValue) {
     replaceValue,
   )
   fs.writeFileSync(filePath, newContents)
+}
+
+function shell(command) {
+  return new Promise((resolve, reject) => {
+    const commandParts = command.split(' ')
+    const cmd = spawn(commandParts[0], commandParts.slice(1), {
+      cwd: process.cwd(),
+      detached: false,
+      stdio: 'inherit',
+    })
+
+    cmd.on('error', reject)
+    cmd.on('close', resolve)
+  })
 }
