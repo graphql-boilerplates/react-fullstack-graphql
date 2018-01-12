ddbf2754bccc4df3f0431ca023981c9b061af954
Nilan Marktanner <nilan.marktanner@gmail.com>
2018-01-12 14:35:46 +0100

    fix(basic): add mutation

--- basic/server/src/index.js
+++ basic/server/src/index.js
@@ -20,6 +20,9 @@ const resolvers = {
         info,
       )
     },
+    deletePost(parent, { id }, ctx, info) {
+      return ctx.db.mutation.deletePost({where: { id } }, info)
+    },
     publish(parent, { id }, ctx, info) {
       return ctx.db.mutation.updatePost(
         {
