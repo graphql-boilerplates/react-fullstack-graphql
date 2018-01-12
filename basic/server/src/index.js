bacdd1d6ea659ce2789083c524b1a3993a4f90c1
Nilan Marktanner <nilan.marktanner@gmail.com>
2018-01-12 14:26:40 +0100

    fix(basic): fix typo

--- basic/server/src/index.js
+++ basic/server/src/index.js
@@ -8,7 +8,7 @@ const resolvers = {
     },
     drafts(parent, args, ctx, info) {
       return ctx.db.query.posts({ where: { isPublished: false } }, info)
-    }
+    },
     post(parent, { id }, ctx, info) {
       return ctx.db.query.post({ where: { id: id } }, info)
     },
