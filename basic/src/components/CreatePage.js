dd82570cb1488299fe898cb3c576d97a0bbceb7d
Devan Beitel <devan@devanb.us>
2018-01-13 16:50:50 -0500

    Updates styles.

--- basic/src/components/CreatePage.js
+++ basic/src/components/CreatePage.js
@@ -1,62 +1,58 @@
-import React from 'react'
-import { withRouter } from 'react-router-dom'
-import { graphql } from 'react-apollo'
-import gql from 'graphql-tag'
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 class CreatePage extends React.Component {
   state = {
     title: '',
-    text: '',
-  }
+    text: ''
+  };
 
   render() {
     return (
-      <div className='w-100 justify-center pa6'>
-        <div className='pa4 flex justify-center bg-white'>
-        <div
-          className='close absolute right-0 top-0 pointer'
-          onClick={this.props.history.goBack}
-        >
-          <img src={require('../assets/close.svg')} alt='' />
-        </div>
-          <div style={{ maxWidth: 700 }} >
-          <div className='black-80 fw3 description no-underline' > Create Draft</div>
+      <div className="w-100 justify-center pa6">
+        <div className="pa4 flex justify-center bg-white">
+          <div className="close absolute right-0 top-0 pointer" onClick={this.props.history.goBack}>
+            <img src={require('../assets/close.svg')} alt="" />
+          </div>
+          <div style={{ maxWidth: 700 }}>
+            <div className="black-80 fw3 description no-underline"> Create Draft</div>
             <input
-              className='w-100 pa2 mv2 br2 b--black-20 bw1'
+              className="w-100 pa2 mv2 br2 b--black-20 bw1"
               style={{ width: 400 }}
               value={this.state.text}
-              placeholder='Title'
+              placeholder="Title"
               onChange={e => this.setState({ text: e.target.value })}
               autoFocus
             />
-            <textarea placeholder='Content' 
+            <textarea
+              placeholder="Content"
               style={{ height: 150 }}
-             value={this.state.title}
+              value={this.state.title}
               onChange={e => this.setState({ title: e.target.value })}
-            className='db w-100 ba bw1 b--black-20 pa2 br2 mb2'></textarea>
+              className="db w-100 ba bw1 b--black-20 pa2 br2 mb2"
+            />
             {this.state.title &&
               this.state.text && (
-                <button
-                  className='pa3 bg-black-10 bn dim ttu pointer'
-                  onClick={this.handlePost}
-                >
+                <button className="pa3 bg-black-10 bn dim ttu pointer" onClick={this.handlePost}>
                   Post
                 </button>
               )}
           </div>
         </div>
-        </div>
-    )
+      </div>
+    );
   }
 
   handlePost = async () => {
-    const { title, text } = this.state
+    const { title, text } = this.state;
     const createDraft = await this.props.createDraftMutation({
-      variables: { title, text },
-    })
-    console.log(createDraft)
-    this.props.history.replace('/')
-  }
+      variables: { title, text }
+    });
+    console.log(createDraft);
+    this.props.history.replace('/');
+  };
 }
 
 const CREATE_DRAFT_MUTATION = gql`
@@ -67,7 +63,7 @@ const CREATE_DRAFT_MUTATION = gql`
       text
     }
   }
-`
+`;
 
 const PUBLISH_MUTATION = gql`
   mutation PublishMutation($id: ID!) {
@@ -76,9 +72,9 @@ const PUBLISH_MUTATION = gql`
       isPublished
     }
   }
-`
+`;
 
 const CreatePageWithMutation = graphql(CREATE_DRAFT_MUTATION, {
-  name: 'createDraftMutation', // name of the injected prop: this.props.createDraftMutation...
-})(CreatePage)
-export default withRouter(CreatePageWithMutation)
+  name: 'createDraftMutation' // name of the injected prop: this.props.createDraftMutation...
+})(CreatePage);
+export default withRouter(CreatePageWithMutation);
--- basic/src/components/DetailPage.js
+++ basic/src/components/DetailPage.js
@@ -1,79 +1,66 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { graphql, compose } from 'react-apollo'
-import { withRouter } from 'react-router-dom'
-import gql from 'graphql-tag'
-
+import React from 'react';
+import { graphql, compose } from 'react-apollo';
+import { withRouter } from 'react-router-dom';
+import gql from 'graphql-tag';
 
 class DetailPage extends React.Component {
   render() {
     if (this.props.postQuery.loading) {
       return (
-        <div className='flex w-100 h-100 items-center justify-center pt7'>
+        <div className="flex w-100 h-100 items-center justify-center pt7">
           <div>Loading (from {process.env.REACT_APP_GRAPHQL_ENDPOINT})</div>
         </div>
-      )
+      );
     }
 
-    const { post } = this.props.postQuery
+    const { post } = this.props.postQuery;
 
-    let action = this._renderAction(post)
+    let action = this._renderAction(post);
 
     return (
-      <div className='w-100 justify-center pa6'>
-        <div
-          className='close absolute right-0 top-0 pointer'
-          onClick={this.props.history.goBack}
-        >
-          <img src={require('../assets/close.svg')} alt='' />
+      <div className="w-100 justify-center pa6">
+        <div className="close absolute right-0 top-0 pointer" onClick={this.props.history.goBack}>
+          <img src={require('../assets/close.svg')} alt="" />
         </div>
-        <div className='items-center black-80 fw3 title '>
+        <div className="items-center black-80 fw3 title ">
           {post.title}
-          <div className='flex black-80 fw3 text mt2 content'>
-            {post.text}
-          </div>
+          <div className="flex black-80 fw3 text mt2 content">{post.text}</div>
         </div>
         {action}
       </div>
-    )
+    );
   }
 
   _renderAction = ({ id, isPublished }) => {
     if (!isPublished) {
       return (
-        <button
-        className='pa3 bg-black-10 bn dim ttu pointer'
-        onClick={() => this.publishDraft(id)}
-      >
-        Publish
-      </button>
-      )
+        <button className="pa3 bg-black-10 bn dim ttu pointer" onClick={() => this.publishDraft(id)}>
+          Publish
+        </button>
+      );
     } else {
       return (
-        <button
-        className='pa3 bg-black-10 bn dim ttu pointer'
-        onClick={(() => this.deletePost(id))}
-      >
-        Delete
-      </button>
-      )
+        <button className="pa3 bg-black-10 bn dim ttu pointer" onClick={() => this.deletePost(id)}>
+          Delete
+        </button>
+      );
     }
-  }
+  };
 
-  deletePost = async (id) => {
+  deletePost = async id => {
     await this.props.deletePost({
-      variables: { id },
-    })
-    console.log('deleted')
-    this.props.history.replace('/')
-  }
+      variables: { id }
+    });
+    console.log('deleted');
+    this.props.history.replace('/');
+  };
 
-  publishDraft = async (id) => {
+  publishDraft = async id => {
     await this.props.publishDraft({
-      variables: { id },
-    })
-    this.props.history.replace('/')
-  }
+      variables: { id }
+    });
+    this.props.history.replace('/');
+  };
 }
 
 const POST_QUERY = gql`
@@ -85,7 +72,7 @@ const POST_QUERY = gql`
       isPublished
     }
   }
-`
+`;
 
 const PUBLISH_MUTATION = gql`
   mutation publish($id: ID!) {
@@ -94,7 +81,7 @@ const PUBLISH_MUTATION = gql`
       isPublished
     }
   }
-`
+`;
 
 const DELETE_MUTATION = gql`
   mutation deletePost($id: ID!) {
@@ -102,7 +89,7 @@ const DELETE_MUTATION = gql`
       id
     }
   }
-`
+`;
 
 export default compose(
   graphql(POST_QUERY, {
@@ -110,15 +97,15 @@ export default compose(
     options: props => ({
       // https://www.apollographql.com/docs/react/basics/queries.html#options-from-props
       variables: {
-        id: props.match.params.id,
-      },
-    }),
+        id: props.match.params.id
+      }
+    })
   }),
   graphql(PUBLISH_MUTATION, {
-    name: 'publishDraft',
+    name: 'publishDraft'
   }),
   graphql(DELETE_MUTATION, {
-    name: 'deletePost',
+    name: 'deletePost'
   }),
-  withRouter,
-)(DetailPage)
+  withRouter
+)(DetailPage);
