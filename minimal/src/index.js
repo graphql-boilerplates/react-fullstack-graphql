b3246349da9bacdb58388a334a84fc146c9ab8d2
Manjula Dube <dube.manjula668@gmail.com>
2018-02-12 22:25:41 +0530

    remove other dependencies since using apollo-client-preset

--- minimal/src/index.js
+++ minimal/src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from 'react-dom';
 import '../src/styles/index.css';
 import App from '../src/components/App';
 import { ApolloProvider } from 'react-apollo';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloClient } from 'apollo-client';
-
+import { HttpLink, InMemoryCache, ApolloClient } from 'apollo-client-preset'
 
 const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
 
