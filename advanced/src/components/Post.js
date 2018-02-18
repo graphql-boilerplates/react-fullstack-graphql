4a57f6addd6f34a1e9c5bc86cc277c0a1893a4ec
Manjula Dube <dube.manjula668@gmail.com>
2018-02-19 00:16:28 +0530

    change the html for post template

--- advanced/src/components/Post.js
+++ advanced/src/components/Post.js
@@ -10,8 +10,19 @@ export default class Post extends React.Component {
 
     return (
       <Link className="no-underline ma1" to={`/post/${this.props.post.id}`}>
-        <h2 className="f3 black-80 fw4 lh-solid">{title}</h2>
-        <p className="black-80 fw3">{this.props.post.text}</p>
+        <article class="bb b--black-10">
+        <a class="db pv4 ph3 ph0-l no-underline black dim" href="#0">
+          <div class="flex flex-column flex-row-ns">
+            <div class="w-100 w-60-ns pl3-ns">
+              <h1 class="f3 fw1 baskerville mt0 lh-title">{ title }</h1>
+              <p class="f6 f5-l lh-copy">
+                {this.props.post.text}
+              </p>
+              <p class="f6 lh-copy mv0">By { this.props.post.author.name }</p>
+            </div>
+          </div>
+        </a>
+      </article>
       </Link>
     )
   }
