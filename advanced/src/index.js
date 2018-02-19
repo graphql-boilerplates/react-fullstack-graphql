2b0cd5346498d73adaee645068a371ae3dbc6b2f
Manjula Dube <dube.manjula668@gmail.com>
2018-02-19 15:35:43 +0530

    add token check on login and logout

--- advanced/src/index.js
+++ advanced/src/index.js
@@ -109,7 +109,7 @@ ReactDOM.render(
           >
             Feed
           </NavLink>
-          <NavLink
+          { token && <NavLink
             className="link dim f6 f5-ns dib mr3 black"
             activeClassName="gray"
             exact={true}
@@ -117,19 +117,24 @@ ReactDOM.render(
             title="Drafts"
           >
             Drafts
-          </NavLink>
-          <Link
+          </NavLink> }
+          {token ? (<Link
             to="/logout"
             className="f6 link dim br1 ba ph3 pv2 fr mb2 dib black"
           >
            Logout
-          </Link>
-          <Link
+          </Link>) : (<Link
+            to="/login"
+            className="f6 link dim br1 ba ph3 pv2 fr mb2 dib black"
+          >
+           Login
+          </Link>) }
+          {token && <Link
             to="/create"
             className="f6 link dim br1 ba ph3 pv2 fr mb2 dib black"
           >
             + Create Draft
-          </Link>
+          </Link> }
         </nav>
         <div className="fl w-100 pl4 pr4">
           <Switch>
