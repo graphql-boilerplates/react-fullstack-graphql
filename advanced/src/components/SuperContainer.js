3d00cf886d45d544931975493e744e11193aa3ac
Manjula Dube <dube.manjula668@gmail.com>
2018-03-03 23:06:54 +0530

    add seperate function

--- advanced/src/components/SuperContainer.js
+++ advanced/src/components/SuperContainer.js
@@ -78,104 +78,110 @@ class SuperContainer extends React.Component {
     return (
       <Router>
         <React.Fragment>
-          <nav className="pa3 pa4-ns">
-            <Link
-              className="link dim black b f6 f5-ns dib mr3"
-              to="/"
-              title="Feed"
-            >
-              Blog
-            </Link>
+          {this.renderNavBar()}
+          {this.renderRoute()}
+        </React.Fragment>
+      </Router>
+    )
+  }
+
+  renderNavBar() {
+    return (
+      <nav className="pa3 pa4-ns">
+        <Link className="link dim black b f6 f5-ns dib mr3" to="/" title="Feed">
+          Blog
+        </Link>
+        <NavLink
+          className="link dim f6 f5-ns dib mr3 black"
+          activeClassName="gray"
+          exact={true}
+          to="/"
+          title="Feed"
+        >
+          Feed
+        </NavLink>
+        {this.props.data &&
+          this.props.data.me &&
+          this.props.data.me.email &&
+          this.state.token && (
             <NavLink
               className="link dim f6 f5-ns dib mr3 black"
               activeClassName="gray"
               exact={true}
-              to="/"
-              title="Feed"
+              to="/drafts"
+              title="Drafts"
             >
-              Feed
+              Drafts
             </NavLink>
-            {this.props.data &&
-              this.props.data.me &&
-              this.props.data.me.email &&
-              this.state.token && (
-                <NavLink
-                  className="link dim f6 f5-ns dib mr3 black"
-                  activeClassName="gray"
-                  exact={true}
-                  to="/drafts"
-                  title="Drafts"
-                >
-                  Drafts
-                </NavLink>
-              )}
-            {this.state.token ? (
-              <div
-                onClick={() => {
-                  this.refreshTokenFn &&
-                    this.refreshTokenFn({
-                      [AUTH_TOKEN]: null,
-                    })
-                  window.location.href = '/'
-                }}
-                className="f6 link dim br1 ba ph3 pv2 fr mb2 dib black"
-              >
-                Logout
-              </div>
-            ) : (
-              <Link
-                to="/login"
-                className="f6 link dim br1 ba ph3 pv2 fr mb2 dib black"
-              >
-                Login
-              </Link>
-            )}
-            {this.props.data &&
-              this.props.data.me &&
-              this.props.data.me.email &&
-              this.state.token && (
-                <Link
-                  to="/create"
-                  className="f6 link dim br1 ba ph3 pv2 fr mb2 dib black"
-                >
-                  + Create Draft
-                </Link>
-              )}
-          </nav>
-          <div className="fl w-100 pl4 pr4">
-            <Switch>
-              <Route exact path="/" component={FeedPage} />
-              <ProtectedRoute
-                token={this.state.token}
-                path="/drafts"
-                component={DraftsPage}
-              />
-              <ProtectedRoute
-                token={this.state.token}
-                path="/create"
-                component={CreatePage}
-              />
-              <Route path="/post/:id" component={DetailPage} />
-              <Route
-                token={this.state.token}
-                path="/login"
-                render={props => (
-                  <LoginPage refreshTokenFn={this.refreshTokenFn} />
-                )}
-              />
-              <Route
-                token={this.state.token}
-                path="/signup"
-                render={props => (
-                  <SignupPage refreshTokenFn={this.refreshTokenFn} />
-                )}
-              />
-              <Route path="/logout" component={LogoutPage} />
-              <Route component={PageNotFound} />
-            </Switch>
+          )}
+        {this.state.token ? (
+          <div
+            onClick={() => {
+              this.refreshTokenFn &&
+                this.refreshTokenFn({
+                  [AUTH_TOKEN]: null,
+                })
+              window.location.href = '/'
+            }}
+            className="f6 link dim br1 ba ph3 pv2 fr mb2 dib black"
+          >
+            Logout
           </div>
-        </React.Fragment>
-      </Router>
+        ) : (
+          <Link
+            to="/login"
+            className="f6 link dim br1 ba ph3 pv2 fr mb2 dib black"
+          >
+            Login
+          </Link>
+        )}
+        {this.props.data &&
+          this.props.data.me &&
+          this.props.data.me.email &&
+          this.state.token && (
+            <Link
+              to="/create"
+              className="f6 link dim br1 ba ph3 pv2 fr mb2 dib black"
+            >
+              + Create Draft
+            </Link>
+          )}
+      </nav>
+    )
+  }
+
+  renderRoute() {
+    return (
+      <div className="fl w-100 pl4 pr4">
+        <Switch>
+          <Route exact path="/" component={FeedPage} />
+          <ProtectedRoute
+            token={this.state.token}
+            path="/drafts"
+            component={DraftsPage}
+          />
+          <ProtectedRoute
+            token={this.state.token}
+            path="/create"
+            component={CreatePage}
+          />
+          <Route path="/post/:id" component={DetailPage} />
+          <Route
+            token={this.state.token}
+            path="/login"
+            render={props => <LoginPage refreshTokenFn={this.refreshTokenFn} />}
+          />
+          <Route
+            token={this.state.token}
+            path="/signup"
+            render={props => (
+              <SignupPage refreshTokenFn={this.refreshTokenFn} />
+            )}
+          />
+          <Route path="/logout" component={LogoutPage} />
+          <Route component={PageNotFound} />
+        </Switch>
+      </div>
     )
   }
 }
