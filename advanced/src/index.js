3777417bb15deee91b8ced2e42130b0df33200cb
Manjula Dube <dube.manjula668@gmail.com>
2018-03-13 11:13:04 +0530

    add http instead of https

--- advanced/src/index.js
+++ advanced/src/index.js
@@ -12,7 +12,7 @@ import 'tachyons'
 import './index.css'
 //for testing purpose
 //const httpLink = new HttpLink({ uri: 'https://advdemo.now.sh' })
-const httpLink = new HttpLink({ uri: 'https://localhost:4000' })
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
 
 const middlewareLink = new ApolloLink((operation, forward) => {
   // get the authentication token from local storage if it exists
