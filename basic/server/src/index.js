f5423cbd1b3fe9638612826efa6c2801ed3b018d
Nikolas Burk <nikolas.burk@gmail.com>
2018-03-23 14:18:51 +0100

    add prisma backend

--- basic/server/src/index.js
+++ basic/server/src/index.js
@@ -42,7 +42,7 @@ const server = new GraphQLServer({
     ...req,
     db: new Prisma({
       typeDefs: 'src/generated/prisma.graphql',
-      endpoint: '__PRISMA_ENDPOINT__',
+      endpoint: 'https://eu1.prisma.sh/nikolas/my-react-basic/dev',
       secret: 'mysecret123',
       debug: true,
     }),
