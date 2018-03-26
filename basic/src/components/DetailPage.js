7474a2a1f71215c39d24206e834959db2e2cd83d
Nikolas <nikolas.burk@gmail.com>
2018-03-26 11:16:05 +0200

    Update DetailPage.js

--- basic/src/components/DetailPage.js
+++ basic/src/components/DetailPage.js
@@ -127,19 +127,6 @@ class DetailPage extends Component {
     return deleteMutation
   }
 
-  deletePost = async id => {
-    await this.props.deletePost({
-      variables: { id },
-    })
-    this.props.history.replace('/')
-  }
-
-  publishDraft = async id => {
-    await this.props.publishDraft({
-      variables: { id },
-    })
-    this.props.history.replace('/')
-  }
 }
 
 const POST_QUERY = gql`
